73c41d72d4f18cfbf706f660be6f7b5b82dd7752
Shubhrajyoti Dey <toshubhrajyotidey@gmail.com>
2025-04-10 18:22:06 +0530

    Added support for GRPC streaming

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,14 +1,16 @@
-use controller::rpc::controller::{controller_client::ControllerClient, AddItemRequest};
+use controller::rpc::controller::{
+    controller_client::ControllerClient, AddItemRequest, ListenRequest,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = ControllerClient::connect("http://[::1]:50051").await?;
 
-    let request = tonic::Request::new(AddItemRequest {
+    let request = tonic::Request::new(ListenRequest {
         ..Default::default()
     });
 
-    let mut stream = client.add_item(request).await?.into_inner();
+    let mut stream = client.listen(request).await?.into_inner();
 
     while let Ok(Some(item_response)) = stream.message().await {
         println!("RESPONSE={:?}", item_response);
--- controller/src/services/controller.rs
+++ controller/src/services/controller.rs
@@ -1,20 +1,36 @@
-use crate::rpc::controller::{controller_server::Controller, AddItemRequest, AddItemResponse};
+use crate::rpc::controller::{
+    controller_server::Controller, AddItemRequest, AddItemResponse, ListenRequest, ListenResponse,
+};
 use anyhow::Result;
+use dashmap::DashMap;
 use redis::client::RedisClient;
-use std::pin::Pin;
-use tokio::sync::mpsc;
+use std::{pin::Pin, sync::Arc, time::Duration};
+use tokio::{
+    spawn,
+    sync::{
+        mpsc::{self, Receiver, Sender},
+        Mutex,
+    },
+    time::sleep,
+};
 use tokio_stream::{wrappers::ReceiverStream, Stream};
 use tonic::{Request, Response, Status};
 
-#[derive(Debug, Clone)]
+use super::task::Task;
+
+#[derive(Debug)]
 pub struct ControllerService {
     pub db: RedisClient,
+    pub num_of_workers: Arc<Mutex<i32>>,
+    pub subscribers: Mutex<DashMap<String, Vec<Sender<Result<ListenResponse, Status>>>>>,
 }
 
 impl ControllerService {
     pub async fn new() -> Result<Self> {
         Ok(Self {
             db: RedisClient::new().await.unwrap(),
+            num_of_workers: Arc::new(Mutex::new(0)),
+            subscribers: Mutex::new(DashMap::new()),
         })
     }
 }
@@ -24,6 +40,7 @@ type StreamResult<T> = Result<Response<T>, Status>;
 #[tonic::async_trait]
 impl Controller for ControllerService {
     type AddItemStream = Pin<Box<dyn Stream<Item = Result<AddItemResponse, Status>> + Send>>;
+    type ListenStream = Pin<Box<dyn Stream<Item = Result<ListenResponse, Status>> + Send>>;
 
     async fn add_item(
         &self,
@@ -41,5 +58,22 @@ impl Controller for ControllerService {
         let output_stream = ReceiverStream::new(rx);
 
         Ok(Response::new(Box::pin(output_stream) as Self::AddItemStream))
+    }
+
+    async fn listen(&self, request: Request<ListenRequest>) -> StreamResult<Self::ListenStream> {
+        let (tx, rx) = mpsc::channel(128);
+        let subscibers = self.subscribers.lock().await;
+        let queue_name = request.into_inner().queue_name;
+        if subscibers.contains_key(&queue_name) {
+            let mut subscibers = subscibers.get_mut(&queue_name).unwrap();
+            subscibers.push(tx);
+        } else {
+            subscibers.insert(queue_name, vec![tx]);
+        }
+        spawn(async { loop {} }); // Keep the client connected
+
+        let output_stream = ReceiverStream::new(rx);
+
+        Ok(Response::new(Box::pin(output_stream) as Self::ListenStream))
     }
 }
